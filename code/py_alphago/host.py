dd1e6cf01c9ab7c9c65b2d46b28d7272c0f2e775
eric-clinch <eric.c.clinch@gmail.com>
2020-05-30 23:21:23 -0400

    Distributed scripts now use a config json

--- code/py_alphago/host.py
+++ code/py_alphago/host.py
@@ -3,6 +3,9 @@ import torch
 import torch.distributed as dist
 from torch.multiprocessing import Process
 import time
+import argparse
+import json
+
 
 def PlayoutListen():
     print("Listening for tensors")
@@ -13,12 +16,30 @@ def PlayoutListen():
         time.sleep(3)
         break
 
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Hosts a distributed pytorch system")
+    parser.add_argument('config_file', type=str, nargs="?",
+                        help="Location of the json config file")
+    args = parser.parse_args()
+
+    with open(args.config_file) as config_file:
+        contents = json.load(config_file)
+
+    if ('addr' not in contents) or ('port' not in contents):
+        print("IP address (addr) and port number required in config")
+
+    address = contents['addr']
+    port = contents['port']
+
     print("Beginning to init group")
+    connection = f"tcp://{address}:{port}"
+    print(f"Hosting at {connection}")
     dist.init_process_group('gloo',
-        init_method='tcp://127.0.0.1:23459',
-        rank=0,
-        world_size=2)
+                            init_method=connection,
+                            rank=0,
+                            world_size=2)
     print("Group init completed")
 
     listen_proc = Process(target=PlayoutListen)

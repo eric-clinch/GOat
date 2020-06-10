2a98304605c283eabb59bec38a7725180cd9cfd5
eric-clinch <eric.c.clinch@gmail.com>
2020-06-09 22:07:40 -0400

    Added more typing annotation to the code

--- code/py_alphago/py_mcts/naive_evaluator.py
+++ code/py_alphago/py_mcts/naive_evaluator.py
@@ -1,5 +1,7 @@
 
-def NaiveEvaluator(board):
+from typing import *
+
+def NaiveEvaluator(board) -> Tuple[float, List[List[float]], float]:
   prior = [[1] * len(board) for _ in range(len(board))]
   pass_prior = 0
   value = board.RandomPlayout() 
--- code/py_alphago/py_mcts/nn_evaluator.py
+++ code/py_alphago/py_mcts/nn_evaluator.py
@@ -8,19 +8,19 @@ from resnet.resnet import Resnet, DEVICE
 from py_mcts.board import Board
 
 
-def GetPlayerStoneMap(board_list, player):
+def GetPlayerStoneMap(board_list: List[List[str]], player: int) -> List[List[float]]:
     player_str = 'B' if player == 0 else 'W'
     def stone_mapper(x): return 1 if x == player_str else 0
     stone_map = [list(map(stone_mapper, row)) for row in board_list]
     return stone_map
 
 
-def BoardToTensor(board: Board):
-    board_list = board.BoardList()
-    player = board.current_player
-    player_stone_map = GetPlayerStoneMap(board_list, player)
-    opponent_stone_map = GetPlayerStoneMap(board_list, 1 - player)
-    network_input = torch.Tensor([player_stone_map, opponent_stone_map])
+def BoardToTensor(board: Board) -> torch.Tensor:
+    board_list: List[List[str]] = board.BoardList()
+    player: int = board.current_player
+    player_stone_map: List[List[float]] = GetPlayerStoneMap(board_list, player)
+    opponent_stone_map: List[List[float]] = GetPlayerStoneMap(board_list, 1 - player)
+    network_input: torch.Tensor = torch.Tensor([player_stone_map, opponent_stone_map])
     return network_input
 
 
@@ -36,7 +36,7 @@ def NNEvaluatorFactory(model_params: Union[str, collections.OrderedDict], board_
         raise Exception("Invalid model parameter object")
     net.train(False)
 
-    def evaluate(board: Board):
+    def evaluate(board: Board) -> Tuple[float, List[List[float]], float]:
         assert(isinstance(board, Board))
         net_input = BoardToTensor(board).unsqueeze(0).to(DEVICE)
         value, policy = net(net_input)
@@ -51,7 +51,7 @@ def NNEvaluatorFactory(model_params: Union[str, collections.OrderedDict], board_
     return evaluate
 
 
-def BatchNNEvaluatorFactory(model_params, board_size):
+def BatchNNEvaluatorFactory(model_params: Union[str, collections.OrderedDict], board_size: int):
     net = Resnet(2, board_size).to(DEVICE)
     if isinstance(model_params, str):
         # Load the params from a file
@@ -63,7 +63,7 @@ def BatchNNEvaluatorFactory(model_params, board_size):
         raise Exception("Invalid model parameter object")
     net.train(False)
 
-    def evaluate(board_batch):
+    def evaluate(board_batch: List[Board]) -> Tuple[List[float], List[List[List[float]]], List[float]]:
         board_size = len(board_batch[0])
         net_input = [BoardToTensor(board) for board in board_batch]
         net_input = torch.stack(net_input, axis=0)
